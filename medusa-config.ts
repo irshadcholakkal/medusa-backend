5d1b75911899f4d8b4a4c057ad28117e6b251774
Irshad Cholakkal <irshadsh12773@gmail.com>
2025-12-21 01:57:52 +0530

    first commit

---
+++ medusa-config.ts
@@ -0,0 +1,33 @@
+// import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+// loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+// module.exports = defineConfig({
+//   projectConfig: {
+//     databaseUrl: process.env.DATABASE_URL,
+//     http: {
+//       storeCors: process.env.STORE_CORS!,
+//       adminCors: process.env.ADMIN_CORS!,
+//       authCors: process.env.AUTH_CORS!,
+//       jwtSecret: process.env.JWT_SECRET || "supersecret",
+//       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+//     }
+//   }
+// })
+
+
+
+
+import { defineConfig } from "@medusajs/framework/utils"
+
+export default defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    http: {
+      storeCors: process.env.STORE_CORS??'',
+      adminCors: process.env.ADMIN_CORS??'',
+      authCors: process.env.AUTH_CORS??'',
+    },
+  },
+})
